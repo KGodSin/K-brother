da8af5488f69df3c0e3bfc496501440858ed690d
kimhyuk <kimhyuk722@live.co.kr>
2020-05-10 15:35:08 +0900

    Add webpack confg (#1)
    * Add - Scss 기본 설정을 세팅한다
    
    Description
    
    - font 기본 스타일 세팅
    - 프로젝트에 필요한 컬러 변수 세팅
    
    * Add WebpackConfig 세팅 및 parcel 제거
    
    - Test 컴포넌트 업데이트
    - webpack.config.js 추가
    - parcel 제거
    - storybook webpack 설정 webpackconfig와 병합
    
    Co-authored-by: 김혁 <kimhyuk@gimhyeog-ui-MacBookPro.local>

--- packages/client/.storybook/main.js
+++ packages/client/.storybook/main.js
@@ -1,14 +1,33 @@
+const webpack = require("../webpack.config");
+
 module.exports = {
 	stories: ["../src/**/*.stories.[tj]sx"],
 	webpackFinal: async config => {
-		config.module.rules.push({
-			test: /\.(ts|tsx)$/,
-			loader: require.resolve("babel-loader"),
-			options: {
-				presets: [["react-app", { flow: false, typescript: true }]]
-			}
-		});
-		config.resolve.extensions.push(".ts", ".tsx");
+		config.module.rules = [...config.module.rules, ...webpack.module.rules];
+		config.plugins = [...config.plugins, ...webpack.plugins];
+		config.resolve = { ...config.resolve, ...webpack.resolve };
+		// config.module.rules.push(
+		// 	{
+		// 		test: /\.s[ac]ss$/i,
+		// 		use: [
+		// 			// Creates `style` nodes from JS strings
+		// 			"style-loader",
+		// 			// Translates CSS into CommonJS
+		// 			"css-loader",
+		// 			"postcss-modules",
+		// 			// Compiles Sass to CSS
+		// 			"sass-loader",
+		// 		],
+		// 	},
+		// 	{
+		// 		test: /\.(ts|tsx)$/,
+		// 		loader: require.resolve("babel-loader"),
+		// 		options: {
+		// 			presets: [["react-app", { flow: false, typescript: true }]],
+		// 		},
+		// 	},
+		// );
+		// config.resolve.extensions.push(".ts", ".tsx");
 		return config;
-	}
+	},
 };
