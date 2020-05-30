0a4600f578d8cfda7b3b4b2eaedafa0d4e3d8647
rladyd818 <40626483+rladyd818@users.noreply.github.com>
2020-05-30 17:23:33 +0900

    Add add class name (#5)
    * Add - Scss 기본 설정을 세팅한다
    
    Description
    
    - font 기본 스타일 세팅
    - 프로젝트에 필요한 컬러 변수 세팅
    
    * Add button (#3)
    
    * scss 문제
    
    * Add WebpackConfig 세팅 및 parcel 제거
    
    - Test 컴포넌트 업데이트
    - webpack.config.js 추가
    - parcel 제거
    - storybook webpack 설정 webpackconfig와 병합
    
    Co-authored-by: 김혁 <kimhyuk@gimhyeog-ui-MacBookPro.local>
    
    * fix branch merge 충돌을 해결한다.
    
    - add-Button, add-webpack-confg 브랜치의 병합충돌 해결
    
    * Add TypographyComponent
    
    - Typography 컴포넌트 추가
    - Storybook의 doc-addon 추가
    - Stroybook의 knobs-addon 추가
    
    * Add util함수 addClassName을 추가한다
    
    - tslint에 interface-over-type-literal:false 추가
    - bablerc에 util경로 추가
    - 입력한 className들을 연결하여 string으로 반환하는 addClassName함수 추가
    
    Co-authored-by: 김혁 <kimhyuk@gimhyeog-ui-MacBookPro.local>

---
+++ packages/client/src/components/Typography/Typography.stories.tsx
@@ -0,0 +1,77 @@
+import * as React from "react";
+import Typography, { TypographyVariant } from "./Typography";
+import { withKnobs, select } from "@storybook/addon-knobs";
+import { ColorProps } from "classes/styles";
+
+const label = {
+	color: "color",
+	textColor: "text-Color",
+	variant: "variant",
+};
+const color = {
+	symbol: "symbol",
+	primary: "primary",
+	secondary: "secondary",
+};
+const textColor = {
+	primary: "primary",
+	secondary: "secondary",
+};
+
+const fontSize = {
+	h1: "h1",
+	h2: "h2",
+	h3: "h3",
+	h4: "h4",
+	h5: "h5",
+	h6: "h6",
+	body1: "body1",
+	body2: "body2",
+	subtitle1: "subtitle1",
+	subtitle2: "subtitle2",
+};
+const defaultValue = {
+	color: "primary",
+	textColor: "primary",
+	variant: "h1",
+};
+
+export default {
+	title: "Typography", // 스토리북에서 보여질 그룹과 경로를 명시
+	component: Typography, // 어떤 컴포넌트를 문서화 할지 명시
+	decorators: [withKnobs], // 애드온 적용
+};
+
+export const withText = () => {
+	const value = select(label.color, color, defaultValue.color) as ColorProps;
+	const value2 = select(
+		label.textColor,
+		textColor,
+		defaultValue.textColor,
+	) as ColorProps;
+	const variant = select(
+		label.variant,
+		fontSize,
+		defaultValue.variant,
+	) as TypographyVariant;
+	return (
+		<Typography variant={variant} color={value} text={value2}>
+			This is h1
+		</Typography>
+	);
+};
+
+// Typo.story = {
+// 	name: "Typography",
+// };
+
+// export const h2 = () => (
+// 	<Typography variant="h2" color="symbol" text="secondary">
+// 		This is h2
+// 	</Typography>
+// );
+// export const h3 = () => (
+// 	<Typography variant="h3" color="symbol" text="secondary">
+// 		This is h3
+// 	</Typography>
+// );
---
+++ packages/client/src/components/Typography/Typography.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import Style from "./Typography.module.scss";
+import { ColorProps } from "classes/styles";
+import ac from "util/addClassName";
+
+export type TypographyVariant =
+	| "h1"
+	| "h2"
+	| "h3"
+	| "h4"
+	| "h5"
+	| "h6"
+	| "body1"
+	| "body2"
+	| "subtitle1"
+	| "subtitle2";
+
+export type TypographyProps = {
+	variant?: TypographyVariant;
+	children: JSX.Element | string;
+	color: ColorProps;
+	text: ColorProps;
+};
+
+export default function Typography({
+	variant,
+	children,
+	color,
+	text,
+}: TypographyProps) {
+	return (
+		<div
+			className={ac(Style[variant], [
+				Style[color + "-color"],
+				Style[text + "-text-color"],
+			])}
+		>
+			{children}
+		</div>
+	);
+}
